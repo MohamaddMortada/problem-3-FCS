3b4f7ef0664801f55a9e81e3d745580ab040e096
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-10-28 00:53:42 +0200

    final commit

--- problem3.js
+++ problem3.js
@@ -1,8 +1,62 @@
+
+function cristmas2() {
+    let r = parseInt(prompt("Input number of rows"));
+    const tree=document.createElement('div');
+    tree.style.textAlign='center';
+    row = document.createElement('div');
+    star = document.createElement('star');
+    star2 = document.createElement('star2');
+    branch = document.createElement('branch');
+
+    star.textContent='*';
+    star.style.color = 'red'; 
+    star.style.fontSize = '20px';
+
+    row.appendChild(star);
+    row.appendChild(branch);
+
+    tree.appendChild(row);
+    document.body.appendChild(tree);
+    i=0;
+    while(i<r-1){  
+    
+    row = document.createElement('div');
+    star = document.createElement('star');
+
+    star2 = document.createElement('star2');
+
+    branch = document.createElement('branch');
+
+    star.textContent='*';
+    star.style.color = 'red'; 
+    star.style.fontSize = '20px';
+    branch.textContent=' | '.repeat(i*2+1);
+    branch.style.color = 'green'; 
+    branch.style.fontSize = '20px';
+    star2.textContent='*';
+    star2.style.color = 'red'; 
+    star2.style.fontSize = '20px';
+    row.appendChild(star);
+    row.appendChild(branch);
+    row.appendChild(star2);
+    tree.appendChild(row);
+    document.body.appendChild(tree);
+    i++;
+    }
+}
+cristmas2();
+
+
+
+////////
 function padding(S,target) {
     const pad = '   '.repeat(target);
     return pad + S;
-}function cristmass(){
+}
+//js
+function cristmas(){
     let r = parseInt(prompt("Enter number of rows"));
+    console.log(r);
     let array='  *';
     console.log(padding(array,r));
     i=0;
@@ -13,4 +67,3 @@ function padding(S,target) {
     }
 
 }
-cristmass();
